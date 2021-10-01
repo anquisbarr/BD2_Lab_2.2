832b42f8c095519093358819792e629c9bda1ed2
Alex Jose Zumaeta <alex.loja@utec.edu.pe>
2021-09-30 23:53:52 -0500

    xd

--- P1/Sequential.cpp
+++ P1/Sequential.cpp
@@ -1,61 +1,117 @@
-#include "Record.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
 
-class Sequential{
-protected:
-    string name;
-    string datafile;
-    string auxfile;
-    int nextDel;
-    char reference;
-    bool empty;
+struct Registro{
+  char codigo[5];
+  char nombre[20];
+  char carrera[15];
+  int ciclo;
+};
+
+struct Page{
+  Registro registro[10];
+};
+
+class Handler{
+  std::vector<Registro> registros;
+  std::unordered_map<std::string, int> indexR;
+  std::string dataFile;
+  std::string indexFile;
+  std::string inputFile;
+
 public:
-    explicit Sequential(string _name, string _auxfile, string _datafile) : name(_name), auxfile(_auxfile), datafile(_datafile) {}
 
-    vector<Record> search(string key){
-        if (key.size() > 20)
-            key = key.substr(0, 20);
-        int pos = bs_lower_bound(key);
-        fstream file (this->name,ios::in | ios::binary);
-        vector<Record> records;
-        int num_records_file = numRecords(file,datafile);
+  Handler(std::string dataFile, std::string indexFile, std::string inputFile) {
+    this->dataFile = dataFile;
+    this->indexFile = indexFile;
+    this->inputFile = inputFile;
+    Registro reg;
+    std::fstream inputStream;
+    inputStream.open(dataFile, std::ios::in | std::ios::binary);
+
+    if(inputStream.is_open()){
+      while(inputStream.read((char*)& reg, sizeof(Registro))) {
+        registros.push_back(reg);
+      }
+		}else
+      std::cout << "Can't open the file";
+  }
+
+  void writeRecord(Page obj) {
+    std::ofstream outFile;
+    outFile.open(dataFile, std::ios::binary | std::ios::app);        
+    outFile.write((char *) &obj, sizeof(obj));
+    outFile.close();
+  }
+
+  void writeIndex() {
+    std::ofstream indexFile;
+    indexFile.open(dataFile, std::ios::binary | std::ios::app);        
+    for(auto &x: indexR){
+      indexFile.write((char *) &x, sizeof(x));
     }
-    
-    void insertAll(vector<Record> &records);
-    bool compareByKey(Record &r1, Record &r2);
-    void isFull();
-    
-    void add(Record record){
-        if (this->empty){
-            fstream fData(this->name, ios::binary | ios::out);
-            initialize_first_values(fData,0,'d');
-            this->empty = false;
-            fData.close();
-            return;
+    indexFile.close();
+  }
+	
+	
+	bool sort_reg(Registro r1, Registro r2){
+    std::string s1 = {r1.codigo[2], r1.codigo[3], r1.codigo[4]};
+    std::string s2 = {r2.codigo[2], r2.codigo[3], r2.codigo[4]};
+
+    if(r1.codigo[1] < r2.codigo[1])
+      return true;
+    else if(r1.codigo[1] > r2.codigo[1])
+      return false;
+    else{
+      return stoi(s1) < stoi(s2);
+    }
+  }
+
+  void readIndex(){
+    //todo
+  }
+
+  void readData(std::string file) {
+		Registro reg;
+		std::fstream infile;
+		infile.open(file, std::ios::in | std::ios::binary);
+		if(infile.is_open()){
+      int c = 0;
+      while(infile.read((char*)& reg, sizeof(Registro))) {
+				registros.push_back(reg);
+        if(c % 10 == 0){
+          std::string temp = reg.codigo;
+          indexR[temp] = c;
         }
-        isFull();
-
-        string k = record.nombre;
-        int first_pos;
-        char first_ref;
-        
-
-    }
+        c++;
+      }
+		}else
+      std::cout << "Can't open the file";
     
-private:
-    
-    int numRecords(fstream& file, string AUX){
-        
+    std::sort(registros.begin(), registros.end(), sort_reg);
+    infile.close();
+  }
+  
+  void insertAll(){
+    std::ofstream salida;
+    salida.open(dataFile, std::ios::app | std::ios::binary);
+    for(auto &x: registros){
+      salida.write((char *)&x, sizeof(Registro));
     }
-
-    void initialize_first_values(fstream& file, int pos, char c){
-        file.open(this->name + ".dat", ios::app);
-        file.seekg(pos * sizeof(Record),ios::beg);
-        file << c;
-    }
-
-    int bs_lower_bound(string key){
-
-    }
-
+    salida.close();
+  }
 
 };
+
+int main(){
+  std::string data = "data.txt";
+  std::string ind = "index.txt";
+  std::string aux = "aux.txt";
+  Handler handler(data, ind, aux);
+  return 0;
+}
